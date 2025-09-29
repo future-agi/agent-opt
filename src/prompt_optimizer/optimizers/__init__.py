bbd8905aaa8980da1f8b2822710685f035cfd70c
Azain Khan <azain@futureagi.com>
2025-09-29 21:42:07 +0530

    add metaprompt optimizer

--- src/prompt_optimizer/optimizers/__init__.py
+++ src/prompt_optimizer/optimizers/__init__.py
@@ -1,4 +1,5 @@
 from .random_search import RandomSearchOptimizer
 from .protegi import ProTeGi
+from .metaprompt import MetaPromptOptimizer
 
-__all__ = ["RandomSearchOptimizer", "ProTeGi"]
+__all__ = ["RandomSearchOptimizer", "ProTeGi", "MetaPromptOptimizer"]
